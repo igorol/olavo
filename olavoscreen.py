4915144312b83f7b2aa34d18cfbd70761b4298b8
igoro <igoro@br.ibm.com>
2017-06-26 12:24:44 -0300

    First commit

---
+++ olavoscreen.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from time import sleep
+from selenium import webdriver
+
+driver = webdriver.PhantomJS()
+driver.set_window_size(325, 500) # set the window size that you need 
+driver.get('olavotweet.html')
+
+sleep(2)
+
+driver.save_screenshot('olavo1.png')
+
