b2d0c35feb66eb2b99c8bb57d0017c338860ee7c
igoro <igoro@br.ibm.com>
2017-06-26 14:44:21 -0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import tweepy
+import os
+from datetime import datetime
+
+consumer_key = os.environ['consumer_key']
+consumer_secret = os.environ['consumer_secret']
+access_token = os.environ['access_token']
+access_token_secret = os.environ['access_token_secret']
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+API = tweepy.API(auth)
+
+API.update_status('testing api {}'.format(datetime.now()))
+
