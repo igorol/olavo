0592005d4dd02a04e4096067353895c08d370ce0
igoro <igoro@br.ibm.com>
2017-06-28 11:02:58 -0300

    More functions

--- olavo.py
+++ olavo.py
@@ -2,6 +2,26 @@ from selenium import webdriver
 from PIL import Image
 import os
 import csv
+import re
+
+
+def check_for_keyword(w, keyword='cu'):
+    word_list = re.split('\W+', w.lower())
+    return keyword in word_list
+
+def publish_tweet(api, status, image = None):
+    """
+
+    :param api: API object
+    :param status: String with status text
+    :param image:  Filename of png to be attached to status
+    :return:
+    """
+
+    if image:
+        api.update_with_media(filename=image, status=status)
+    else:
+        api.update_status(status=status)
 
 
 def screenshot_tweet(id_scr):
@@ -19,23 +39,20 @@ def screenshot_tweet(id_scr):
     # getting element containing the tweet body
     # element = driver.find_element_by_class_name('js-original-tweet')
     element = driver.find_element_by_class_name('permalink-tweet')
-    print element
     location = element.location
     size = element.size
-    print size
-    print location
     driver.save_screenshot('full_screenshot_{}.png'.format(id_scr))
     driver.quit()
 
     ## cropping the full screenshot around desired element
     im = Image.open('full_screenshot_{}.png'.format(id_scr))  # uses PIL library to open image in memory
-    left = location['x'] + 10
-    top = location['y']
-    right = location['x'] + size['width'] + 120
-    bottom = location['y'] + size['height']
+    left = location['x'] + 160
+    top = location['y'] + 40
+    right = location['x'] + size['width'] + 140
+    bottom = location['y'] + size['height'] + 20
     im = im.crop((left, top, right, bottom))  # defines crop points
     im.save('screenshot_{}.png'.format(id_scr))
-    ## os.remove('full_screenshot_{}.png'.format(id_scr))
+    os.remove('full_screenshot_{}.png'.format(id_scr))
 
 
 def retrieve_all_tweets(api, id_scr):
@@ -46,20 +63,20 @@ def retrieve_all_tweets(api, id_scr):
     :param id_scr: Twitter user id, in string format
     :return:
     """
-    alltweets = []
+    full_tweet_list = []
     new_tweets = api.user_timeline(user_id=id_scr, count=200)
-    alltweets.extend(new_tweets)
-    oldest = alltweets[-1].id - 1
+    full_tweet_list.extend(new_tweets)
+    oldest = full_tweet_list[-1].id - 1
 
     while len(new_tweets) > 0:
         print "getting tweets before {}".format(oldest)
         new_tweets = api.user_timeline(user_id=id_scr, count=200, max_id=oldest)
-        alltweets.extend(new_tweets)
-        oldest = alltweets[-1].id - 1
+        full_tweet_list.extend(new_tweets)
+        oldest = full_tweet_list[-1].id - 1
 
-    out_tweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8")] for tweet in alltweets]
+    out_tweets = [[tweet.id_str, tweet.created_at, tweet.text.encode("utf-8"), tweet.entities] for tweet in full_tweet_list]
 
     with open('{}_tweets.csv'.format(id_scr), 'wb') as f:
         writer = csv.writer(f)
-        writer.writerow(["id", "created_at", "text"])
+        writer.writerow(["id", "created_at", "text", "entities"])
         writer.writerows(out_tweets)
